7321c6886567283b60693a4f37438feee802c333
mrfachri19 <fachryfachry1997@gmail.com>
2023-08-14 00:46:32 +0700

    update

--- src/modules/user/userRoutes.js
+++ src/modules/user/userRoutes.js
@@ -3,10 +3,11 @@ const express = require("express");
 const Router = express.Router();
 
 const userController = require("./userController");
-const middlewareAuth = require("../../middleware/auth");
-const middlewareUpload = require("../../middleware/uploadUser");
+// const middlewareAuth = require("../../middleware/auth");
+// const middlewareUpload = require("../../middleware/uploadUser");
 
-Router.get("/:id", middlewareAuth.authentication, userController.getUserById);
+Router.get("/:id", userController.getUserById);
+Router.get("/", userController.getAllUsers);
 Router.patch("/:id", userController.updateProfile);
 // Router.patch(
 //   "/update-image/:id",
